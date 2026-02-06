c195504d66f8b6f2fb5db94bcea23fb7d2fae57d
buiducminh <minh0358583908@gmail.com>
2026-02-06 20:54:55 +0700

    Add files via upload

---
+++ quaivat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class QuaiVat
+{
+    public string Ten { get; set; }
+    public int Mau { get; set; }
+    public string MauSac { get; set; }
+
+    public QuaiVat(string ten, int mau, string mauSac)
+    {
+        Ten = ten;
+        Mau = mau;
+        MauSac = mauSac;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, QuaiVat> dsQuaiVat = new Dictionary<string, QuaiVat>();
+
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.WriteLine($"Nhap quai vat thu {i}");
+
+            Console.Write("Nhap ID: ");
+            string id = Console.ReadLine();
+
+            Console.Write("Nhap ten quai vat: ");
+            string ten = Console.ReadLine();
+            Console.Write("Nhap mau (HP): ");
+            int mau = int.Parse(Console.ReadLine());
+            Console.Write("Nhap mau sac: ");
+            string mauSac = Console.ReadLine();
+            dsQuaiVat.Add(id, new QuaiVat(ten, mau, mauSac));
+            Console.WriteLine();
+        }
+
+        Console.Write("Nhap id quai vat can tim: ");
+        string idTim = Console.ReadLine();
+        if (dsQuaiVat.ContainsKey(idTim))
+        {
+            QuaiVat qv = dsQuaiVat[idTim];
+            Console.WriteLine("Ten: " + qv.Ten);
+            Console.WriteLine("Mau: " + qv.Mau);
+            Console.WriteLine("Mau sac: " + qv.MauSac);
+        }
+        else
+        {
+            Console.WriteLine("Khong tim thay quai vat ???");
+        }
+    }
+}
---
+++ vukhi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VuKhi
+{
+    public string Ten { get; set; }
+    public int SatThuong { get; set; }
+    public VuKhi(string ten, int satThuong)
+    {
+        Ten = ten;
+        SatThuong = satThuong;
+    }
+    public void TanCong()
+    {
+        Console.WriteLine($"Vu khi {Ten} gay ra {SatThuong} sat thuong!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<VuKhi> khoVuKhi = new List<VuKhi>();
+        int tongSatThuong = 0;
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.WriteLine($"Nhap vu khi thu {i}");
+            Console.Write("Nhap ten vu khi: ");
+            string ten = Console.ReadLine();
+            Console.Write("Nhap sat thuong: ");
+            int satThuong = int.Parse(Console.ReadLine());
+            VuKhi vk = new VuKhi(ten, satThuong);
+            khoVuKhi.Add(vk);
+            tongSatThuong += satThuong;
+            Console.WriteLine();
+        }
+
+        Console.WriteLine(" DANH SACH VU KHI");
+        foreach (VuKhi vk in khoVuKhi)
+        {
+            vk.TanCong();
+        }
+        Console.WriteLine(" Tong sat thuong cua vu khi: " + tongSatThuong);
+    }
+}
